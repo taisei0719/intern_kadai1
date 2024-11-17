b8ee70b0f8a14691ceef729e2fec6ed249cd6025
taisei0719 <taisei_10.01@icloud.com>
2024-11-18 00:46:15 +0900

    multi-page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import "./App.css"
 
-function MyButton() {
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import { Home } from "./components/Home";
+import { Page1 } from "./components/Page1";
+import { Page2 } from "./components/Page2";
+import { Page3 } from "./components/Page3";
+
+function App() {
   return (
-    <button>
-      I'm a button
-    </button>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+        <Link to="/page3">Page3</Link>
+        <br />
+
+        <Switch>
+          {/* exactをつけると完全一致になります。Homeはexactをつけてあげます */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/page1">
+            <Page1 />
+          </Route>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+          <Route path="/page3">
+            <Page3 />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
-}
+export default App;
