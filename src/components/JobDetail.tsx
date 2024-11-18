35ca1daac4ca9f74f266dc7419a90eac03066ef3
taisei0719 <taisei_10.01@icloud.com>
2024-11-18 13:03:56 +0900

    a

--- src/components/JobDetail.tsx
+++ src/components/JobDetail.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Job } from "../types";
 
-const mockJobs: Job[] = [
-  { id: 1, title: "ソフトウェアエンジニア", location: "東京", description: "Webアプリ開発。" },
-  { id: 2, title: "プロジェクトマネージャー", location: "大阪", description: "プロジェクト管理業務。" },
-  { id: 3, title: "データアナリスト", location: "リモート", description: "データ解析。" },
+const jobList : Job[] = [
+  { id: 1, title: "フロントエンドエンジニア", category: "エンジニア", income:600},
+  { id: 2, title: "バックエンドエンジニア", category: "営業", income:350},
+  { id: 3, title: "プロジェクトマネージャー", category: "マーケティング", income:800},
 ];
 
 export const JobDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const job = mockJobs.find((job) => job.id === parseInt(id || ""));
+  const job = jobList.find((job) => job.id === parseInt(id || ""));
 
   if (!job) {
     return <p>求人情報が見つかりません。</p>;
@@ -19,8 +19,8 @@ export const JobDetail: React.FC = () => {
   return (
     <div>
       <h2>{job.title}</h2>
-      <p>勤務地: {job.location}</p>
-      <p>{job.description}</p>
+      <p>カテゴリ: {job.category}</p>
+      <p>年収: {job.income}万円</p>
     </div>
   );
 };
